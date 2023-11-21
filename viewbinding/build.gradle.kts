815aab97cff87039807e6b19dcc0c862551de375
HuairenWu <106858061+HuairenWu@users.noreply.github.com>
2023-11-21 18:38:49 +0800

    update

--- viewbinding/build.gradle.kts
+++ viewbinding/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.20-1.0.14")
-//    implementation("com.squareup:kotlinpoet-ksp:1.15.1")
+    implementation("com.squareup:kotlinpoet-ksp:1.15.1")
 }
--- viewbinding/src/main/java/com/google/dagger/MySymbolProcessorProvider.kt
+++ viewbinding/src/main/java/com/google/dagger/MySymbolProcessorProvider.kt
@@ -2,6 +2,14 @@ package com.google.dagger
 
 import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.*
+import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.ksp.writeTo
 import java.io.File
 import kotlin.concurrent.thread
 import kotlin.random.Random
@@ -24,51 +32,64 @@ class MySymbolProcessor(private val environment: SymbolProcessorEnvironment) : S
     override fun process(resolver: Resolver): List<KSAnnotated> {
 
         thread(true) {
-
             val viewBindingBuildDirectory = environment.options["viewBindingBuildDirectory"] ?: throw IllegalStateException()
-
             val directory = File(viewBindingBuildDirectory)
             val fileList = mutableListOf<File>()
             listFilesRecursively(directory, fileList)
             fileList.forEach {
                 environment.logger.warn(it.name)
             }
-
-            val outputStream = environment.codeGenerator.createNewFile(Dependencies.ALL_FILES, "com.google.dagger.di", "ViewBindingModule")
-            outputStream.write(
-                """
-            package com.google.dagger.di
-
-            import android.content.Context
-            import android.view.LayoutInflater
-            import com.google.dagger.databinding.ActivityMainBinding
-            import dagger.Module
-            import dagger.Provides
-            import dagger.hilt.InstallIn
-            import dagger.hilt.android.components.ActivityComponent
-            import dagger.hilt.android.qualifiers.ActivityContext
-
-
-            @Module
-            @InstallIn(ActivityComponent::class)
-            class ViewBindingModel {
-
-          
-                @Provides
-                fun provideActivityMainBinding(layoutInflater: LayoutInflater): ActivityMainBinding {
-                    return ActivityMainBinding.inflate(layoutInflater)
-                }
-
-            }
-        """.trimIndent().encodeToByteArray()
-            )
-            outputStream.flush()
-            outputStream.close()
-
+            val fileSpec = generateViewBindingModule()
+            fileSpec.writeTo(environment.codeGenerator, Dependencies(false))
         }
 
-
         return emptyList()
+    }
+
+
+    fun generateViewBindingModule(): FileSpec {
+        val packageName = "com.google.dagger.di"
+        val className = "ViewBindingModel"
+
+        // Create file builder
+        val fileBuilder = FileSpec.builder(packageName, className)
+
+        // Create module annotation
+        val moduleAnnotation = AnnotationSpec.builder(ClassName("dagger", "Module")).build()
+
+        // Create installIn annotation
+        val installInAnnotation = AnnotationSpec.builder(ClassName("dagger.hilt", "InstallIn"))
+            .addMember("%T::class", ClassName("dagger.hilt.android.components", "ActivityComponent"))
+            .build()
+
+        val providesAnnotation = AnnotationSpec.builder(ClassName("dagger", "Provides"))
+            .build()
+
+
+        // Create class builder
+        val classBuilder = TypeSpec.classBuilder(className)
+            .addAnnotation(moduleAnnotation)
+            .addAnnotation(installInAnnotation)
+//            .addModifiers(KModifier.PUBLIC)
+
+        // Create provide method
+        val provideMethod = FunSpec.builder("provideActivityMainBinding")
+            .addAnnotation(providesAnnotation)
+//            .addModifiers(KModifier.PUBLIC)
+            .addParameter("layoutInflater", ClassName("android.view", "LayoutInflater"))
+            .returns(ClassName("com.google.dagger.databinding", "ActivityMainBinding"))
+            .addStatement("return %T.inflate(layoutInflater)", ClassName("com.google.dagger.databinding", "ActivityMainBinding"))
+
+        // Add provide method to class builder
+        classBuilder.addFunction(provideMethod.build())
+
+        // Add class builder to file builder
+        fileBuilder.addType(classBuilder.build())
+
+        // Write the file
+        return fileBuilder.build()
+
+
     }
 
     private fun listFilesRecursively(directory: File, fileList: MutableList<File>) {
